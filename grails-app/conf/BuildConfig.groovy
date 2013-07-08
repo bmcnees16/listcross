ca2ac35e7831c3e58da60cbbdf6e6b9dede8e107
John Meanor <jmeanor@gmail.com>
2013-07-08 01:16:07 -0400

    Grails 2.2.2 Upgrade & Security Fix
    Upgraded grails to 2.2.2 and fixed Spring security bug where login page
    labels were not displayed.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -42,6 +42,9 @@ grails.project.dependency.resolution = {
         runtime ":resources:1.1.5"
 		
 		compile ":webxml:1.4.1"
+        //compile ":cloud-foundry:1.2.3"
+        compile ":spring-security-core:1.2.7.3"
+
 		
         build ":tomcat:$grailsVersion"
     }
